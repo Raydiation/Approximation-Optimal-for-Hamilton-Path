97b1310316ff7551c0b3b00a3ec7c45fa4628139
Raydiation <86969611+Raydiation@users.noreply.github.com>
2021-08-17 18:01:24 +0800

    Update TSP.cpp
    Add ACO algorithm

--- TSP.cpp
+++ TSP.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 #include"SA.cpp"
 #include"Brute_Force.cpp"
+#include"ACO.cpp"
 using namespace std;
 class TSP_solution
 {
@@ -10,25 +11,29 @@ public:
     {
         /*Brute_Force Brute_Force_solver(Graph);
         Brute_Force_solver.Force_solution();
-        return Brute_Force_solver.goodOrder;*/
+        return Brute_Force_solver.goodOrder;
         int max_round=1000;
         SA SA_solver(Graph,max_round);
         SA_solver.main_algorithm(max_round);
-        return SA_solver.goodOrder;
+        return SA_solver.goodOrder;*/
+        ACO ACOsolver(Graph,50);
+        ACOsolver.init();
+        ACOsolver.Round(0);
+        return ACOsolver.goodPath;
     }
     vector<vector<int>> Graph;
 };
 int main()
 {
-    //my test case
     default_random_engine generator = default_random_engine(time(NULL));
     uniform_int_distribution<int> distribution_int(0,100);
     auto rand_int=bind(distribution_int,generator);
 
-    vector<vector<int>> graph(8,vector<int>(8));
-    for(int i=0;i<8;i++)
+    const int node_num=18;
+    vector<vector<int>> graph(node_num,vector<int>(node_num));
+    for(int i=0;i<node_num;i++)
     {
-        for(int j=0;j<8;j++)
+        for(int j=0;j<node_num;j++)
         {
             if(i==j) graph[i][j]=0;
             else if(i>j)graph[i][j]=graph[j][i];
@@ -36,17 +41,14 @@ int main()
         }
     }
 
-    for(int i=0;i<8;i++)
+    for(int i=0;i<node_num;i++)
     {
-        for(int j=0;j<8;j++)
+        for(int j=0;j<node_num;j++)
         {
             cout<<graph[i][j]<<" ";
         }
         cout<<endl;
     }
-    //my test case
     TSP_solution solve(graph);
-    vector<int> ans=solve.TSP();
-    for(int num:ans)
-        cout<<num<<" ";
+    solve.TSP();
 }
